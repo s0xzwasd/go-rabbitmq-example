7976a331ec5f4b88b9edbe8dd94270648e5a8354
Daniil Maslov <i@dmaslov.dev>
2021-03-25 18:22:15 +0300

    Add basic connection implementation of RabbitMQ

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/s0xzwasd/go-rabbitmq-example
+
+go 1.16
+
+require github.com/streadway/amqp v1.0.0
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
 
 func main() {
-	fmt.Println("Go RabbitMQ")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ instance")
 }
